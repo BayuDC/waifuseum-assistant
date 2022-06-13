3012a1ed754f2ff12dd432883ab2b70fc900d4ca
BayuDC <dev.bayudc@gmail.com>
2022-06-13 19:33:15 +0700

    feat: ping api and show logs

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from os import getenv
 from discord import Client, Message, TextChannel
 from discord.ext import tasks
+from datetime import datetime
+from requests import head
 
 token = getenv('BOT_TOKEN')
 prefix = getenv('BOT_PREFIX') or '!'
+api_url = getenv('API_URL')
 
 client = Client()
 
 
-@tasks.loop(seconds=1)
+@tasks.loop(minutes=1)
 async def task(channel: TextChannel):
-    await channel.send('Looping!')
+    try:
+        now = datetime.now().strftime('%Y-%m-%d %H:%M')
+        response = head(api_url)
+
+        if response.status_code == 200:
+            await channel.send(f'`[{now}]` Waifuseum api is **up!**')
+        else:
+            await channel.send(f'`[{now}]` Waifuseum api is **down!**')
+    except Exception as e:
+        print(e)
 
 
 @client.event
@@ -26,6 +38,8 @@ async def on_message(message: Message):
         case 'say':
             await message.channel.send(' '.join(args))
         case 'start':
+            if api_url is None:
+                return await message.channel.send('`API_URL` is not set!')
             if task.is_running():
                 return await message.channel.send('Task is already running!')
 
@@ -37,7 +51,8 @@ async def on_message(message: Message):
 
             await message.channel.send('Stoping task...')
             task.stop()
-
+        case 'status':
+            await task(message.channel)
         case _:
             await message.channel.send('Unknown command!')
 
